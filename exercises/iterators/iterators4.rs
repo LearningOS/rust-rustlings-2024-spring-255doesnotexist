4c60722d833d46df09d629f3f8680989955a2877
255doesnotexist <chenyejin2004@stumail.ysu.edu.cn>
2024-04-10 10:38:21 +0800

    不使用递归、传统循环、新增变量、return的阶乘实现。有点酷了。我不懂函数式编程，但是有点像。

--- exercises/iterators/iterators4.rs
+++ exercises/iterators/iterators4.rs
@@ -3,8 +3,6 @@
 // Execute `rustlings hint iterators4` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 pub fn factorial(num: u64) -> u64 {
     // Complete this function to return the factorial of num
     // Do not use:
@@ -15,6 +13,14 @@ pub fn factorial(num: u64) -> u64 {
     // For an extra challenge, don't use:
     // - recursion
     // Execute `rustlings hint iterators4` for hints.
+
+    match num {
+        0 => 1,
+        _ => (1..=num).fold(1, |acc, x| acc * x),
+    }
+    // fold is a method that applies a function to each element of an iterator and accumulates the results 
+    // acc is the always the accumulator, the value that is returned and passed to the next iteration
+    // x is each element in the container 
 }
 
 #[cfg(test)]
