dc1a32e8272bc059bd8ae933ef0e63eaed45f84c
255doesnotexist <1293883574jcy@gmail.com>
2024-04-12 09:37:32 +0800

    implement a stack with double queues

--- exercises/algorithm/algorithm8.rs
+++ exercises/algorithm/algorithm8.rs
@@ -2,7 +2,6 @@
 	queue
 	This question requires you to use queues to implement the functionality of the stac
 */
-// I AM NOT DONE
 
 #[derive(Debug)]
 pub struct Queue<T> {
@@ -52,6 +51,8 @@ impl<T> Default for Queue<T> {
     }
 }
 
+
+// 两个队列实现一个栈，经典 
 pub struct myStack<T>
 {
 	//TODO
@@ -68,14 +69,28 @@ impl<T> myStack<T> {
     }
     pub fn push(&mut self, elem: T) {
         //TODO
+        self.q1.enqueue(elem);
     }
     pub fn pop(&mut self) -> Result<T, &str> {
         //TODO
-		Err("Stack is empty")
+		match self.q1.size() {
+            0 => Err("Stack is empty"),
+            _ => {
+                while self.q1.size() > 1 {
+                    self.q2.enqueue(self.q1.dequeue().unwrap());
+                }
+                let result = self.q1.dequeue().unwrap();
+                std::mem::swap(&mut self.q1, &mut self.q2);
+                Ok(result)
+            },
+        }
     }
     pub fn is_empty(&self) -> bool {
 		//TODO
-        true
+		match self.q1.size() {
+            0 => true,
+            _ => false,
+        }
     }
 }
 
