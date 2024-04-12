69a8936d7abe270453c6927e69b84ac9b33f35c8
255doesnotexist <1293883574jcy@gmail.com>
2024-04-12 10:35:42 +0800

    undirected graph with hashmap (associated array form)

--- exercises/algorithm/algorithm10.rs
+++ exercises/algorithm/algorithm10.rs
@@ -1,8 +1,8 @@
 /*
 	graph
-	This problem requires you to implement a basic graph functio
+	This problem requires you to implement a basic graph function
 */
-// I AM NOT DONE
+// just easily implemented a undirected graph. 嘻嘻。 
 
 use std::collections::{HashMap, HashSet};
 use std::fmt;
@@ -29,7 +29,14 @@ impl Graph for UndirectedGraph {
         &self.adjacency_table
     }
     fn add_edge(&mut self, edge: (&str, &str, i32)) {
-        //TODO
+        self.adjacency_table_mutable()
+            .entry(String::from(edge.0))
+            .or_insert(Vec::new())
+            .push((String::from(edge.1), edge.2));
+        self.adjacency_table_mutable()
+            .entry(String::from(edge.1))
+            .or_insert(Vec::new())
+            .push((String::from(edge.0), edge.2));
     }
 }
 pub trait Graph {
@@ -37,11 +44,14 @@ pub trait Graph {
     fn adjacency_table_mutable(&mut self) -> &mut HashMap<String, Vec<(String, i32)>>;
     fn adjacency_table(&self) -> &HashMap<String, Vec<(String, i32)>>;
     fn add_node(&mut self, node: &str) -> bool {
-        //TODO
+        self.adjacency_table_mutable().insert(String::from(node), Vec::new());
 		true
     }
     fn add_edge(&mut self, edge: (&str, &str, i32)) {
-        //TODO
+        self.adjacency_table_mutable()
+            .entry(String::from(edge.0))
+            .or_insert(Vec::new())
+            .push((String::from(edge.1), edge.2));
     }
     fn contains(&self, node: &str) -> bool {
         self.adjacency_table().get(node).is_some()
